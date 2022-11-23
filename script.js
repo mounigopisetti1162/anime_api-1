4fb90a657a87ed0c691ec53ceaa5b0020ec70376
mouni <mounika.g1162@gmail.com>
2022-11-23 17:57:16 +0530

    ANIMATIONS

---
+++ script.js
@@ -0,0 +1,31 @@
+document.body.innerHTML=`<div class="animi"> <h4>ANIMATION</h4>
+<img class="icon" src="https://tse3.mm.bing.net/th?id=OIP.IAU4rRu7fzzBuoJvPTRRkgHaD4&pid=Api&P=0">
+</div>
+<div id="main"></div>
+`
+const getdata=async()=>
+{
+    try {
+        
+   
+    let animi=await fetch("https://anime-facts-rest-api.herokuapp.com/api/v1")
+    let value=await animi.json()
+   let output=(value.data)
+    output.forEach((obj)=>
+    {
+        console.log(obj)
+        display(obj)
+    })
+}
+ catch (error) {
+        alert("not found")
+}
+}
+
+function display(obj)
+{
+    main.innerHTML+=`<div class="name">Name:<h4>${obj.anime_name}</h4>
+    <img class="image" src=${obj.anime_img}>
+    </div>`
+}
+getdata()
